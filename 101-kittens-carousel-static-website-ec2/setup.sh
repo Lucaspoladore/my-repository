e68b577ee0a71376122c40fe8fcd99c3a1d896b5
Lucaspoladore <lucas.spoladore@vtex.com>
2024-08-19 21:12:25 -0400

    html change

--- 101-kittens-carousel-static-website-ec2/setup.sh
+++ 101-kittens-carousel-static-website-ec2/setup.sh
@@ -5,7 +5,7 @@
 
 cd /var/www/html
 
-FOLDER=https://raw.githubusercontent.com/altazbhanji/my-repository/main/101-kittens-carousel-static-website-ec2/static-web
+FOLDER=https://raw.githubusercontent.com/lucaspoladore/my-repository/main/101-kittens-carousel-static-website-ec2/static-web
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
 wget ${FOLDER}/cat1.jpg
